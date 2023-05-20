70ca985347a79e8d43215f2859e4d306d6d2f9db
WEI TSE <curry900312@gmail.com>
2023-05-20 16:37:19 +0800

    revise basename

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/notes">
       <App />
     </BrowserRouter>
   </React.StrictMode>
